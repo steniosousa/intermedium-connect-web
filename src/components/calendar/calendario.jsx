5e17a1ef62b45ec6572eaf5cafb643ef8be266b6
Stenio Sousa <steniosousaf@gmail.com>
2023-08-23 14:49:55 -0300

    fix: cron finalized

--- src/components/calendar/calendario.jsx
+++ src/components/calendar/calendario.jsx
@@ -1,6 +1,39 @@
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-function App({ children }) {
+
+export default function Calendario({ children }) {
+    const localizer = momentLocalizer(moment);
+    const [selectedDates, setSelectedDates] = useState([]);
+
+    const handleSelect = ({ start, end }) => {
+      const selectedRange = [];
+      let currentDate = start;
+  
+      while (currentDate <= end) {
+        selectedRange.push(currentDate);
+        currentDate = moment(currentDate).add(1, 'days').toDate();
+      }
+  
+      setSelectedDates(selectedRange);
+    };
   return (
-   <></>
+    <div style={{ height: 500 }}>
+    <Calendar
+      localizer={localizer}
+      selectable
+      events={[]}
+      onSelectSlot={handleSelect}
+      selected={selectedDates}
+    />
+    <div>
+      <h3>Dias Selecionados:</h3>
+      {selectedDates.map((date, index) => (
+        <p key={index}>{moment(date).format('YYYY-MM-DD')}</p>
+      ))}
+    </div>
+  </div>
   );
 }
--- src/components/solicitation/index.jsx
+++ src/components/solicitation/index.jsx
@@ -5,11 +5,29 @@ import Api from '../../api/service';
 import Alert from '../alert';
  
 export default function Solicitation({ showModal, onClose,datas }) {
-  const [selectedEnvironment, setSelectedEnvironment] = useState('');
   const [ objectsOptions, setObjectsOptions] = useState([])
   const [placesOptions, setPlacesOptions] = useState([])
   const [selectForSend, setSelectForSend] = useState([])
   const [placesForSend, setPlacesForSend] = useState('')
+  const [daysOfWeek, setDaysOfWeek] = useState([
+    {dia:"Segunda-feita",value:1},
+    {dia:"Terça-feita",value:2},
+    {dia:"Quarta-feita",value:3},
+    {dia:"Quinta-feita",value:4},
+    {dia:"Sexta-feita",value:5},
+    {dia:"Sabado",value:6},
+    {dia:"Domingo",value:7},
+  ])
+  const [horsOfDay,setHorsOfDay] = useState([
+    '01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24'
+  ])
+
+  const [ repeat, setRepeat] = useState(false)
+
+  const [daySelected, setDaySelected] = useState('')
+  const[horsSelected, setHorsSelected] = useState('')
+
+  const [automated, setAutomated] = useState(null)
 
     const[showAlert, setShowAlert] = useState(false)
     const[messengerAlert, setMessengerAlert] = useState('')
@@ -48,15 +66,16 @@ export default function Solicitation({ showModal, onClose,datas }) {
 
 
   const handleObjectChange = (event) => {
+    if(event.target.value == '') return
     const exist = selectForSend.find((item) => item.id == event.target.value)
     if(exist) return
     const idSelect = objectsOptions.find((item) => item.id == event.target.value)
     setSelectForSend([...selectForSend,idSelect])
   };
   const handlePlacesChange = (event) =>{
+    if(event.target.value == '') return
     const idSelect = placesOptions.find((item) => item.id == event.target.value)
     setPlacesForSend(idSelect.name)
-    console.log(idSelect)
   }
 
  
@@ -65,24 +84,50 @@ export default function Solicitation({ showModal, onClose,datas }) {
     setSelectForSend(exclusion)
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if(placesForSend === '' || selectForSend.length == 0){
+
+    if(placesForSend === '' || selectForSend.length == 0 ){
       setShowAlert(true)
       setMessengerAlert('Erro ao gravar solicitação, verifique se os campos então preenchidos')
       return
     }
     const objects = []
+
     for(const object of selectForSend){
         objects.push(object.id)
     }
     const send = {
         userId:datas.id,
-        where:selectedEnvironment,
+        where:placesForSend,
         objects
     }
+    const sendOfAutomated = {
+      userId:datas.id,
+      where:placesForSend,
+      objects,
+      daySelected,
+      horsSelected,
+      repeat
+    }
+    if(automated){
+      console.log(selectForSend,placesForSend,placesForSend,horsSelected)
+      if(placesForSend === '' || selectForSend.length == 0|| horsSelected === ''){
+        setShowAlert(true)
+        setMessengerAlert('Erro ao gravar solicitação, verifique se os campos então preenchidos')
+        return
+      }
+      try{
+        await Api.post('cleaning/cron', sendOfAutomated)
+        setMessengerAlert('Solicitação de automação cadastrada com sucesso')
+        setShowAlert(true)
+      }catch(error){
+        console.log(error)
+      }
+      return 
+    }
     try{
-        Api.post('cleaning',send)
+       await Api.post('cleaning',send)
         setMessengerAlert('Solicitação cadastrada com sucesso')
         setShowAlert(true)
     }
@@ -96,6 +141,18 @@ export default function Solicitation({ showModal, onClose,datas }) {
   function openAlert(){
     handleClose()
     setShowAlert(!showAlert)
+  }
+
+  function handleSelectDay(event){
+    setDaySelected(event.target.value)
+  }
+
+  function handleSelectHors(event){
+    setHorsSelected(event.target.value)
+  }
+
+  function handleRepeat(event){
+    setRepeat(!repeat)
   }
 
 
@@ -127,14 +184,13 @@ export default function Solicitation({ showModal, onClose,datas }) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-          <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl m-4 transition-all sm:my-8 sm:w-full max-w-sm">
+          <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl m-4 transition-all sm:my-8 sm:w-full max-w-max">
 
               <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                 <div className="flex flex-col items-center justify-center">
                  
                   <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                   <Dialog.Title as="h3" className="text-base leading-6 text-gray-900">
-                  {/* <span className="font-semibold">Usuário:</span> {datas.name} */}
                 </Dialog.Title>
   
 
@@ -145,8 +201,8 @@ export default function Solicitation({ showModal, onClose,datas }) {
                         Ambiente:
                         </label>
                         <select className="border rounded-md px-3 py-2 w-full" onChange={handlePlacesChange}>
-                        <option value="" disabled>
-                            Selecione uma opção
+                        <option value="">
+                            Selecione um ambiente
                         </option>
                         {placesOptions.map((item) =>{
                             return(
@@ -155,19 +211,13 @@ export default function Solicitation({ showModal, onClose,datas }) {
                         })}
                         </select>
                     </div>
-                    {placesForSend?(
-
-                    <button className="bg-green-600 text-white font-semibold px-4 py-2 rounded-full cursor-pointer" >
-                                {placesForSend}
-                            </button>
-                    ):null}
                     <div className="mb-4">
                         <label htmlFor="object" className="block text-gray-700 font-semibold mb-1">
                         Objeto:
                         </label>
                         <select className="border rounded-md px-3 py-2 w-full" onChange={handleObjectChange}>
-                        <option value="" disabled>
-                            Selecione uma opção
+                        <option value="" >
+                            Selecione um objeto
                         </option>
                         {objectsOptions.map((item) =>{
                             return(
@@ -181,26 +231,72 @@ export default function Solicitation({ showModal, onClose,datas }) {
                     <Dialog.Title as="h3" className="text-base leading-6 text-gray-900 flex flex-row items-center justify-around m-4 ">
                     {selectForSend.map((item) =>{
                         return(
-                            <button className="bg-red-600 text-white font-semibold px-4 py-2 rounded-full cursor-pointer" key={item.id} onClick={()=>handleExclusion(item)}>
+                            <button type='button' className="bg-red-600 text-white font-semibold px-4 py-2 rounded-full cursor-pointer" key={item.id} onClick={()=>handleExclusion(item)}>
                                 {item.name}
                             </button>
                   )
                 })}
                 </Dialog.Title>
-               <BasicDateCalendar/>
+               {automated?(
+                <div className='flex flex-row items-center'>
+               <div className="mb-4">
+                        <label htmlFor="object" className="block text-gray-700 font-semibold mb-1">
+                        Dia da semana:
+                        </label>
+                        <select className="border rounded-md px-3 py-2 w-40" onChange={handleSelectDay}>
+                        <option value="" >
+                             Dia
+                        </option>
+                        {daysOfWeek.map((item) =>{
+                            return(
+                                <option  value={item.value}>{item.dia}</option>
+
+                            )
+                        })}
+                        </select>
+                </div>
+                <div className="mb-4">
+                        <label htmlFor="object" className="block text-gray-700 font-semibold mb-1">
+                        Horário:
+                        </label>
+                        <select className="border rounded-md px-3 py-2 w-40" onChange={handleSelectHors}>
+                        <option value="">
+                            Horário
+                        </option>
+                        {horsOfDay.map((item) =>{
+                            return(
+                                <option  value={item}>{item}</option>
+
+                            )
+                        })}
+                        </select>
+
+                </div>
+                        <div class="flex flex-col  items-center m-4 dark:border-gray-700">
+                          <input onChange={handleRepeat} id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox" class=" text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                          <label for="bordered-checkbox-1" class=" text-sm font-medium text-gray-600 dark:text-gray-600">Repeat</label>
+                      </div>
+                </div>
+
+
+               ):null}
+
                 < div className='flex flex-row  justify-around w-full '>
+                <button
+                    type='button'
+                        onClick={() => setAutomated(!automated)}
+                        className="bg-gray-600 hover:bg-black text-white py-2 px-4 rounded-md font-semibold"
+                    >
+                        Automatizar
+                    </button>
                     <button
                         type="submit"
                         className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md font-semibold"
                     >
                         Enviar
                     </button>
-                    <button
-                        className="bg-violet-500 hover:bg-black text-white py-2 px-4 rounded-md font-semibold"
-                    >
-                        Automatizar
-                    </button>
 
+                    
                 </div>
                 </form>
                   </div>
@@ -210,7 +306,7 @@ export default function Solicitation({ showModal, onClose,datas }) {
                 <button
                   type="button"
                   className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                  onClick={handleClose} // Use a função de fechamento aqui
+                  onClick={handleClose} 
                   ref={cancelButtonRef}
                 >
                   Fechar
